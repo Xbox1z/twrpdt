8b6e056e1463a461663de33963399fdaba5ae564
Xbox1z <m18088056205@163.com>
2023-08-06 19:59:04 +0800

    a

---
+++ exc/vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's #MODE# device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo #MODE#_#DEVICE#-user
+add_lunch_combo #MODE#_#DEVICE#-userdebug
+add_lunch_combo #MODE#_#DEVICE#-eng
